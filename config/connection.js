22277e0be96d3ac2934b37d3a260d55587050ff2
Bannywalia <Banny.walia07@gmail.com>
2022-11-28 20:46:16 -0500

    added routes

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,8 @@
+const connection = require('sequelize');
 const Sequelize = require('sequelize');
 require('dotenv').config();
+const fs = require("fs");
+const seedQuery = fs.readFileSync("db/seed.sql", {encoding:"utf-8",})
 
 const sequelize = new Sequelize(
     process.env.DB_NAME,
@@ -11,5 +14,11 @@ const sequelize = new Sequelize(
         port: 3301,
     }
 );
-
+connection.query(seedQuery,err => {
+    if(err){
+        throw err
+    }
+    console.log("seeded")
+    connection.end()
+})
 module.exports = sequelize;
--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,15 @@
 DROP DATABASE IF EXISTS workout_db;
 CREATE DATABASE workout_db;
 
+USE workout_db;
+
+CREATE TABLE workouts(
+  today VARCHAR(30) NOT NULL,
+  workout_id INT NOT NULL,
+  excercise VARCHAR(100) NOT NULL,
+  duration INT NOT NULL,
+  weight INT NOT NULL,
+  sets INT NOT NULL,
+  reps INT NOT NULL
+);
+
